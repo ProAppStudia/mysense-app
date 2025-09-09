3198d578d42a5385865b8f09c60a72714a4e3e9b
ProAppStudia <proapp.studia@gmail.com>
2025-09-09 18:41:29 +0300

    first api change Home

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ElementRef } from '@angular/core';
 import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonContent, IonButton],
+  imports: [IonContent, IonButton, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit, AfterViewInit {
+  @ViewChild('articlesSwiper')
+  swiperEl?: ElementRef;
+
+  homepageData: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getHomepageData();
+  }
+
+  ngAfterViewInit() {
+    if (this.swiperEl) {
+      this.swiperEl.nativeElement.addEventListener('swiperinit', () => {
+        if (this.swiperEl && this.swiperEl.nativeElement.swiper) {
+          this.swiperEl.nativeElement.swiper.update();
+        }
+      });
+    }
+  }
+
+  getHomepageData() {
+    this.http.get('https://mysense.care/app/connector.php?action=get_homepage').subscribe((data) => {
+      this.homepageData = data;
+      console.log(this.homepageData);
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { home, filterOutline, flaskOutline, chatbubblesOutline, personOutline } from 'ionicons/icons';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -14,5 +15,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
   ],
 });
