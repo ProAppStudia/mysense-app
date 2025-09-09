97ac912973683541b900fb6cb57b0f03ec2741b3
ProAppStudia <proapp.studia@gmail.com>
2025-09-09 23:28:41 +0300

    home firsts sectiond

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,62 @@
 import { Component, OnInit, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, ViewChild, ElementRef } from '@angular/core';
-import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonButton, IonAccordionGroup, IonAccordion, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { register } from 'swiper/element/bundle';
 
 register();
 
+interface Doctor {
+  img: string;
+  firstname: string;
+  practice_years_text: string;
+}
+
+interface HomepageData {
+  section_1: {
+    heading: string;
+    sub_heading: string;
+    button_test_text: string;
+    text_choise_psyhologist: string;
+  };
+  doctors: Doctor[];
+  section_3: {
+    heading_acquaintance: string;
+    text_acquaintance: string;
+  };
+  section_8: {
+    heading: string;
+    sub_heading: string;
+    cities: { name: string }[];
+    img: string;
+  };
+  section_9: {
+    heading: string;
+    reviews: { text: string; date: string; user_name: string }[];
+  };
+  section_10: {
+    heading: string;
+    items: { heading: string; content: string }[];
+    text_button: string;
+  };
+  section_11: {
+    heading: string;
+    articles: { title: string }[];
+  };
+}
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonContent, IonButton, CommonModule],
+  imports: [IonContent, IonButton, CommonModule, IonAccordionGroup, IonAccordion, IonItem, IonLabel],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Tab1Page implements OnInit, AfterViewInit {
   @ViewChild('articlesSwiper')
   swiperEl?: ElementRef;
 
-  homepageData: any;
+  homepageData: HomepageData | null = null;
 
   constructor(private http: HttpClient) {}
 
@@ -36,7 +75,7 @@ export class Tab1Page implements OnInit, AfterViewInit {
   }
 
   getHomepageData() {
-    this.http.get('https://mysense.care/app/connector.php?action=get_homepage').subscribe((data) => {
+    this.http.get<HomepageData>('https://mysense.care/app/connector.php?action=get_homepage').subscribe((data) => {
       this.homepageData = data;
       console.log(this.homepageData);
     });
