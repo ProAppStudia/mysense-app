a3eb187d57a961cd9ba3b7e7bfc80b8166b321e3
ProAppStudia <proapp.studia@gmail.com>
2025-09-10 16:17:41 +0300

    profile icons

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { home, filterOutline, flaskOutline, chatbubblesOutline, personOutline } from 'ionicons/icons';
+import { home, filterOutline, flaskOutline, chatbubblesOutline, personOutline, calendarOutline, addCircleOutline, bookOutline, libraryOutline, informationCircleOutline, notificationsOutline, headsetOutline, documentTextOutline, logOutOutline } from 'ionicons/icons';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-addIcons({ home, filterOutline, flaskOutline, chatbubblesOutline, personOutline });
+addIcons({ home, filterOutline, flaskOutline, chatbubblesOutline, personOutline, calendarOutline, addCircleOutline, bookOutline, libraryOutline, informationCircleOutline, notificationsOutline, headsetOutline, documentTextOutline, logOutOutline });
 
 bootstrapApplication(AppComponent, {
   providers: [
