6b0b0795d90f5b058860c977e8419c0ed1efc033
ProAppStudia <proapp.studia@gmail.com>
2025-09-10 16:53:32 +0300

    if login or not

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { home, filterOutline, flaskOutline, chatbubblesOutline, personOutline, calendarOutline, addCircleOutline, bookOutline, libraryOutline, informationCircleOutline, notificationsOutline, headsetOutline, documentTextOutline, logOutOutline, eyeOutline, eyeOffOutline, closeOutline } from 'ionicons/icons';
+import { home, filterOutline, flaskOutline, chatbubblesOutline, personOutline, calendarOutline, addCircleOutline, bookOutline, libraryOutline, informationCircleOutline, notificationsOutline, headsetOutline, documentTextOutline, logOutOutline, eyeOutline, eyeOffOutline, closeOutline, helpCircleOutline, warningOutline } from 'ionicons/icons';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-addIcons({ home, filterOutline, flaskOutline, chatbubblesOutline, personOutline, calendarOutline, addCircleOutline, bookOutline, libraryOutline, informationCircleOutline, notificationsOutline, headsetOutline, documentTextOutline, logOutOutline, eyeOutline, eyeOffOutline, closeOutline });
+addIcons({ home, filterOutline, flaskOutline, chatbubblesOutline, personOutline, calendarOutline, addCircleOutline, bookOutline, libraryOutline, informationCircleOutline, notificationsOutline, headsetOutline, documentTextOutline, logOutOutline, eyeOutline, eyeOffOutline, closeOutline, helpCircleOutline, warningOutline });
 
 bootstrapApplication(AppComponent, {
   providers: [
