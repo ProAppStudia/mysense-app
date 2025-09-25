eea4554faa14084c02fcb6e143d7c54f74c40d3a
ProAppStudia <proapp.studia@gmail.com>
2025-09-25 18:37:01 +0300

    chats 1.0

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonList, IonItem, IonAvatar, IonLabel, IonFooter, IonInput } from '@ionic/angular/standalone';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonIcon, IonList, IonItem, IonAvatar, IonLabel, IonFooter, IonInput]
 })
 export class ChatPage implements OnInit {
+  chats: any[] = [];
+  selectedChat: any = null;
+  messages: any[] = [];
 
-  constructor() { }
+  constructor(private chatService: ChatService) { }
 
   ngOnInit() {
+    this.chatService.getMyChats().subscribe({
+      next: (data: any) => {
+        if (Array.isArray(data)) {
+          this.chats = data;
+        } else if (data && data.chats) {
+          this.chats = data.chats;
+        }
+      },
+      error: (error) => {
+        // Handle error appropriately in a real app
+      }
+    });
   }
 
+  selectChat(chat: any) {
+    this.selectedChat = chat;
+    this.chatService.getChatMessages(chat.chat_id).subscribe({
+      next: (data: any) => {
+        if (Array.isArray(data)) {
+          this.messages = data;
+        } else if (data && data.messages) {
+          this.messages = data.messages;
+        }
+      },
+      error: (error) => {
+        // Handle error appropriately in a real app
+      }
+    });
+  }
+
+  goBack() {
+    this.selectedChat = null;
+    this.messages = [];
+  }
 }
---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { TokenStorageService } from './token-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+  private apiUrl = `${environment.baseUrl}/connector.php`;
+
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) { }
+
+  getMyChats(): Observable<any> {
+    const token = this.tokenStorage.getToken();
+    return this.http.get(`${this.apiUrl}?action=get_my_chats&token=${token}`);
+  }
+
+  getChatMessages(chatId: number): Observable<any> {
+    const token = this.tokenStorage.getToken();
+    return this.http.get(`${this.apiUrl}?action=get_my_chat_messages&chat_id=${chatId}&token=${token}`);
+  }
+}
