37efcfd6d6e0a4a5ff0b7668fb9976774e5109e1
ProAppStudia <proapp.studia@gmail.com>
2025-11-09 10:23:16 +0200

    7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,23 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'mysense.app',
   appName: 'mysense-app',
-  webDir: 'www',
-
-  server: {
-    // Дозволяємо навігацію/запити з WebView на ці домени
-    allowNavigation: ['mysense.care', 'www.mysense.care']
-    // dev-server тут не потрібен (тільки для ng serve)
-  },
-
-  ios: {
-    // Стандартна схема — 'capacitor'; міняти не потрібно
-    scheme: 'capacitor',
-    contentInset: 'always'
-  },
-
-  android: {
-    backgroundColor: '#FFFFFF'
-  }
+  webDir: 'www'
 };
 
 export default config;
