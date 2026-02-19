9f63188ffb4872ec761e9ccca8bd7b7b9d0ba7e7
ProAppStudia <proapp.studia@gmail.com>
2026-02-19 19:34:16 +0200

    preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnDestroy {
+  isPreloaderHiding = false;
+  isPreloaderHidden = false;
+
+  private hideTimer: ReturnType<typeof setTimeout>;
+  private removeTimer: ReturnType<typeof setTimeout>;
+
+  constructor() {
+    this.hideTimer = setTimeout(() => {
+      this.isPreloaderHiding = true;
+    }, 2200);
+
+    this.removeTimer = setTimeout(() => {
+      this.isPreloaderHidden = true;
+    }, 2700);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.hideTimer);
+    clearTimeout(this.removeTimer);
+  }
 }
